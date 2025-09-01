f01df624fbd61b5f4dbf91f59163cdc01d24cc96
Julia D Brancher <juliadocter@hotmail.com>
2025-08-31 21:39:37 -0300

    Correção bg

--- core/Background.py
+++ core/Background.py
@@ -1,10 +1,13 @@
 from core.Const import EnvEnum, EntityEnum
 from core.Entity import Entity
+import pygame
 
 
 class Background(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.surf = pygame.transform.scale(self.surf, EnvEnum.WINDOW_SIZE)
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
 
     def move(self):
         self.rect.centerx -= EntityEnum.SPEED[self.name]
